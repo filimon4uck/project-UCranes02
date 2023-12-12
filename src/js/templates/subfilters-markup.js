60af41a280907f5f45c67d7a60a23e4a58ddc6be
Maxim <kmaxusk@gmail.com>
2023-12-12 21:43:36 +0200

    corrections for array of exercises

--- src/js/templates/subfilters-markup.js
+++ src/js/templates/subfilters-markup.js
@@ -1,12 +1,11 @@
 function subfiltersMarkup(array) {
-  return array
-    .map(
+    return array.map(
       ({
         filter,
         name,
-        imgUrl,
+        imgURL,
       }) => `<li data-subfilter="${name}" class="exercises-item">
-  <img class="exercises-item-image" src="${imgUrl}" alt="${name}" />
+  <img class="exercises-item-image" width="300" src="${imgURL}" alt="${name}" />
   <p class="exercises-item-name">${name}</p>
   <p class="exercises-item-type">${filter}</p>
 </li>
