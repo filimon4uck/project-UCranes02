661cbd6d27a1fb6a83683d88805e3b7fa837923c
Maxim <kmaxusk@gmail.com>
2023-12-12 21:18:50 +0200

    add exercises markup

--- src/js/templates/subfilters-markup.js
+++ src/js/templates/subfilters-markup.js
@@ -1,5 +1,18 @@
 function subfiltersMarkup(array) {
-  return 'Hello World';
+  return array
+    .map(
+      ({
+        filter,
+        name,
+        imgUrl,
+      }) => `<li data-subfilter="${name}" class="exercises-item">
+  <img class="exercises-item-image" src="${imgUrl}" alt="${name}" />
+  <p class="exercises-item-name">${name}</p>
+  <p class="exercises-item-type">${filter}</p>
+</li>
+`
+    )
+    .join('');
 }
 
 export default subfiltersMarkup;
