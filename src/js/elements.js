7e89353351d7f90982fc7085c79cc83ca7516f02
lexandr10 <alexandrenko3006@gmail.com>
2023-12-13 10:05:57 -0500

    create exercises markup

--- src/js/elements.js
+++ src/js/elements.js
@@ -2,6 +2,7 @@ const elements = {
   burger: document.querySelector('.js-burger'),
   filter: document.querySelector('.js-filter'),
   gallery: document.querySelector('.js-gallery'),
+  btnExercise: document.querySelector('[data-filter="bodypart"]'),
 };
 
 export { elements };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import 'modern-normalize';
-
+import exercisesMarkup from './js/templates/exercises-markup';
 import { ExercisesAPI } from './js/services/exercises-api';
 import { elements } from './js/elements';
 import { subfiltersMarkup } from './js/templates';
 import { handleFilter, handleSubfilter, handleExercise } from './js/handlers';
 const api = new ExercisesAPI({});
 
-elements.filter.addEventListener('click', handleFilter)
-elements.gallery.addEventListener('click', handleSubfilter)
-elements.gallery.addEventListener('click', handleExercise)
-
+elements.filter.addEventListener('click', handleFilter);
+elements.gallery.addEventListener('click', handleSubfilter);
+elements.gallery.addEventListener('click', handleExercise);
+elements.btnExercise.addEventListener('click', handleBodypart);
 async function renderSubfilters() {
   const subFilters = await api.getFilters();
   elements.gallery.innerHTML = subfiltersMarkup(subFilters.results);
