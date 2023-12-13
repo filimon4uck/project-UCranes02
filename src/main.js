259d21817b675c96a62d71a6bef9ae907471da59
Yulia <nayanzina@gmail.com>
2023-12-13 18:09:21 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@ import 'modern-normalize';
 import { ExercisesAPI } from './js/services/exercises-api';
 import { elements } from './js/elements';
 import { subfiltersMarkup } from './js/templates';
-import { handleFilter, handleSubfilter, handleExercise } from './js/handlers';
+import {
+  handleFilter,
+  handleSubfilter,
+  handleExercise,
+  pageSwitch,
+} from './js/handlers';
 const api = new ExercisesAPI({});
 
-elements.filter.addEventListener('click', handleFilter)
-elements.gallery.addEventListener('click', handleSubfilter)
-elements.gallery.addEventListener('click', handleExercise)
+// elements.filter.addEventListener('click', handleFilter);
+elements.gallery.addEventListener('click', handleSubfilter);
+elements.gallery.addEventListener('click', handleExercise);
 
 async function renderSubfilters() {
   const subFilters = await api.getFilters();
   elements.gallery.innerHTML = subfiltersMarkup(subFilters.results);
 }
 
+pageSwitch();
 renderSubfilters();
