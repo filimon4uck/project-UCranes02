0d4dc23f507279c6bbfa217f009f16c569c2b87f
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-13 20:18:37 +0200

    add handler filters

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 import 'modern-normalize';
 
-import { ExercisesAPI } from './js/services/exercises-api';
+import { exercisesApi } from './js/services/exercises-api';
 import { elements } from './js/elements';
 import { subfiltersMarkup } from './js/templates';
 import { handleFilter, handleSubfilter, handleExercise } from './js/handlers';
-const api = new ExercisesAPI({});
 
 elements.filter.addEventListener('click', handleFilter)
 elements.gallery.addEventListener('click', handleSubfilter)
 elements.gallery.addEventListener('click', handleExercise)
 
 async function renderSubfilters() {
-  const subFilters = await api.getFilters();
+  const subFilters = await exercisesApi.getFilters();
   elements.gallery.innerHTML = subfiltersMarkup(subFilters.results);
 }
 
