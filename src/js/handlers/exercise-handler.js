594cdd6b615e9c34ecd34bce37ef58ce80e4b35f
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-14 14:24:51 +0200

    update

--- src/js/handlers/exercise-handler.js
+++ src/js/handlers/exercise-handler.js
@@ -1,7 +1,5 @@
 import { showSuccess } from '../helpers/toaster';
 import { exercisesApi } from '../services/exercises-api';
-import { exerciseDetailsMarkup } from '../templates';
-import { handleFavorites } from '../handlers';
 
 async function handleExercise(e) {
   e.preventDefault();
@@ -11,12 +9,6 @@ async function handleExercise(e) {
   const data = await exercisesApi.getExerciseById(exerciseId);
   console.log(data);
   showSuccess(exerciseId);
-  const exerciseMarkup = exerciseDetailsMarkup(data);
-  document
-    .querySelector('footer')
-    .insertAdjacentHTML('afterend', exerciseMarkup);
-
-  handleFavorites(data);
 }
 
 export default handleExercise;
