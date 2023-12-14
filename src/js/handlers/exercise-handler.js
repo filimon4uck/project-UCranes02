c9f53fa31ba49c3bb9c471caf2c4d2cd354a91f0
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-14 13:14:23 +0200

    refactoring

--- src/js/handlers/exercise-handler.js
+++ src/js/handlers/exercise-handler.js
@@ -1,6 +1,5 @@
 import { showSuccess } from '../helpers/toaster';
 import { exercisesApi } from '../services/exercises-api';
-import { exerciseDetailsMarkup } from '../templates';
 
 async function handleExercise(e) {
   e.preventDefault();
@@ -10,26 +9,6 @@ async function handleExercise(e) {
   const data = await exercisesApi.getExerciseById(exerciseId);
   console.log(data);
   showSuccess(exerciseId);
-  const exerciseMarkup = exerciseDetailsMarkup(data);
-  document
-    .querySelector('footer')
-    .insertAdjacentHTML('afterend', exerciseMarkup);
 }
-// console.log(exerciseDetailsMarkup());
-// const exMarkup = exerciseDetailsMarkup({
-//   bodyPart: 'waist',
-//   equipment: 'body weight',
-//   gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0002.gif',
-//   name: '45° side bend',
-//   target: 'abs',
-//   description:
-//     "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
-//   rating: 4.33,
-//   burnedCalories: 323,
-//   time: 3,
-//   popularity: 754,
-// });
-
-// document.querySelector('footer').insertAdjacentHTML('afterend', exMarkup);
 
 export default handleExercise;
