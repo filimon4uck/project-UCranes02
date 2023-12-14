7677454672f495d1862914f9e40043815aed72cb
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-12-14 15:51:50 +0200

    refactored

--- src/js/handlers/exercise-handler.js
+++ src/js/handlers/exercise-handler.js
@@ -1,18 +1,20 @@
 import { showSuccess, showError } from '../helpers/toaster';
 import { exercisesApi } from '../services/exercises-api';
 import { exerciseDetailsMarkup } from '../templates';
+import handleFavorites from './add-favorites-handler';
 
 const popUpState = {
   detailsPopup: false,
   ratingPopup: false,
 };
 
-const handleListeners = (detailsPopupHtml) => {
+const stopPropagation = (e) => e.stopPropagation();
+
+const getPopupElements = (markup) => {
   const container = document.createElement('div');
-  container.innerHTML = detailsPopupHtml;
+  container.innerHTML = markup;
 
   const backdrop = container.querySelector('.exercise-modal-backdrop');
-
   const detailsPopup = container.querySelector('.exercise-modal');
   const detailsCloseBtn = container.querySelector('.exercise-card-close-btn');
   const ratingBtn = container.querySelector('.add-rating-btn');
@@ -21,37 +23,58 @@ const handleListeners = (detailsPopupHtml) => {
   const _ratingPopupBackdrop = document.querySelector('[data-modal]');
   const ratingPopup = _ratingPopupBackdrop.querySelector('.modal').cloneNode(true);
 
+  return { backdrop, detailsPopup, detailsCloseBtn, ratingBtn, favoriteButton, ratingPopup };
+};
+
+const handleRatingPopup = (ratingPopup) => {
+  const closeButton = ratingPopup.querySelector('.modal__close-btn');
+  const ratingForm = ratingPopup.querySelector('.give_a_rating');
+};
+
+const handleListeners = (detailsPopupHtml, data) => {
+  const {
+    backdrop,
+    detailsPopup,
+    detailsCloseBtn,
+    ratingBtn,
+    favoriteButton,
+    ratingPopup,
+  } = getPopupElements(detailsPopupHtml);
+
+  const closeDetailsPopup = () => {
+    popUpState.detailsPopup = false;
+    popUpState.ratingPopup = false;
+    backdrop?.remove();
+  };
+
+  const closeRatingPopup = () => {
+    ratingPopup.remove();
+    backdrop.innerHTML = '';
+    popUpState.ratingPopup = false;
+    backdrop.append(detailsPopup);
+  };
+
   const handleBackdropClickAndEsc = () => {
-    
-    // debugger;
     switch (true) {
       case popUpState.detailsPopup && !popUpState.ratingPopup:
-        popUpState.detailsPopup = false;
-        popUpState.ratingPopup = false;
-        backdrop.remove();
+        closeDetailsPopup();
         return;
       case popUpState.detailsPopup && popUpState.ratingPopup:
-        ratingPopup.remove();
-        backdrop.innerHTML = '';
-        popUpState.ratingPopup = false;
-        backdrop.append(detailsPopup);
+        closeRatingPopup();
         return;
       default:
-        popUpState.detailsPopup = false;
-        popUpState.ratingPopup = false;
-        backdrop?.remove();
+        closeDetailsPopup();
         return;
     }
   }
 
-  window.addEventListener('keydown', e => {
+  window.addEventListener('keydown', (e) => {
     if (e.code !== 'Escape') return;
     handleBackdropClickAndEsc();
   });
 
-  detailsPopup.addEventListener('click', e => {
-    e.stopPropagation();
-  });
+  detailsPopup.addEventListener('click', stopPropagation);
+  ratingPopup.addEventListener('click', stopPropagation);
 
   backdrop.addEventListener('click', () => {
     handleBackdropClickAndEsc();
@@ -67,6 +90,12 @@ const handleListeners = (detailsPopupHtml) => {
     backdrop.append(ratingPopup);
   });
 
+  favoriteButton.addEventListener('click', (e) => {
+    handleFavorites(data);
+  });
+
+  handleRatingPopup(ratingPopup, closeRatingPopup);
+
   popUpState.detailsPopup = true;
   document.body.append(backdrop);
 }
@@ -79,18 +108,14 @@ async function handleExercise(e, ratingPopupCallback) {
     const exerciseId = e.target.closest('[data-id]').dataset.id;
 
     const data = await exercisesApi.getExerciseById(exerciseId);
-    console.log(data);
 
-    showSuccess(exerciseId);
     const detailsPopup = exerciseDetailsMarkup(data);
-    const modalElement = handleListeners(detailsPopup);
+    const modalElement = handleListeners(detailsPopup, data);
   
   } catch (err) {
-    console.log(err);
-    showError(err.message);
+    console.error(err);
+    showError("Something went wrong. Please try again later.");
   }
-
-
 }
 
 export default handleExercise;
