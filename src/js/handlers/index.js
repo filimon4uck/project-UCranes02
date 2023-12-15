712e2ed6a3b3655ad8281a2616666fe68ea5f26c
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-15 06:57:37 +0200

    add css for page change

--- src/js/handlers/index.js
+++ src/js/handlers/index.js
@@ -1,7 +1,6 @@
 import handleFilter from './filter-handler';
 import handleSubfilter from './subfilter-handler';
 import handleExercise from './exercise-handler';
-import handleNavigation from './navigation-handler';
 import handlePagination from './pagination-handler';
 import handleFavorites from './add-favorites-handler';
 import handleSubscribe from './subscribe-handler';
@@ -10,7 +9,6 @@ export {
   handleFilter,
   handleSubfilter,
   handleExercise,
-  handleNavigation,
   handlePagination,
   handleFavorites,
   handleSubscribe,
