5725a6a4ff4dba3e7c2635fe169fc2aa32ae1672
MarznaHuzei <marinka777xx@gmail.com>
2023-12-15 16:12:42 +0100

    script rating modal

--- src/js/templates/rating-modal.js
+++ src/js/templates/rating-modal.js
@@ -1,60 +1,20 @@
-const modal = document.querySelector('[data-modal]');
-const modalCloseButtons = document.querySelectorAll('[data-modal-close]');
-const modalOpenButtons = document.querySelectorAll('[data-modal-open]');
-const ratingForm = document.querySelector('.give_a_rating');
+import { showError } from '../helpers/toaster';
+import { exercisesApi } from '../services/exercises-api';
 
-document.addEventListener('DOMContentLoaded', function () {
-  function openModal() {
-    modal.classList.remove('is-hidden');
+export default async function submitForm(e, onSuccess) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  try {
+    const resp = await exercisesApi.updateExerciseRating({
+      rate: Number(formData.get('rate')),
+      email: formData.get('email'),
+      review: formData.get('review'),
+    });
+
+    onSuccess();
+  } catch (error) {
+    console.log(error);
+    showError();
   }
-
-  function closeModal() {
-    modal.classList.add('is-hidden');
-  }
-
-  function submitForm(event) {
-    event.preventDefault();
-
-    // Get input values
-    const userEmail = document.getElementById('user_email_up').value;
-    const userComment = document.getElementById('user_comment').value;
-    const userRating = document.querySelector('input[name="rating"]:checked');
-
-    // Store values in local storage
-    localStorage.setItem('userEmail', userEmail);
-    localStorage.setItem('userComment', userComment);
-    localStorage.setItem(
-      'userRating',
-      userRating ? userRating.value : 'no rating'
-    );
-
-    // For demonstration purposes, let's log the stored values
-    console.log('Stored Email:', localStorage.getItem('userEmail'));
-    console.log('Stored Comment:', localStorage.getItem('userComment'));
-    console.log('Stored Rating:', localStorage.getItem('userRating'));
-
-    closeModal();
-  }
-
-  modalOpenButtons.forEach(function (button) {
-    button.addEventListener('click', openModal);
-  });
-
-  modalCloseButtons.forEach(function (button) {
-    button.addEventListener('click', closeModal);
-  });
-
-  window.addEventListener('click', function (event) {
-    if (event.target === modal) {
-      closeModal();
-    }
-  });
-
-  window.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-      closeModal();
-    }
-  });
-
-  ratingForm.addEventListener('submit', submitForm);
-});
+}
