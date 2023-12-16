594394def12777d0f28875e18d85ac18d2f8c8a3
Yulia <nayanzina@gmail.com>
2023-12-17 01:04:39 +0200

    fix

--- src/favorites.js
+++ src/favorites.js
@@ -3,7 +3,20 @@ import 'modern-normalize';
 import exercisesMarkup from './js/templates/exercises-markup';
 import { elements } from './js/elements';
 import { common } from './js/common';
+import removeFavorites from './js/helpers/favorites/remove-favorites-handler';
+import handleExercise from './js/handlers/exercise-handler';
 
-const cards = JSON.parse(localStorage.getItem(common.LS_KEY_FAVORITES))  ?? [];
+const cards = JSON.parse(localStorage.getItem(common.LS_KEY_FAVORITES)) ?? [];
 
-elements.gallery.innerHTML = exercisesMarkup(cards, 'favorites')
+elements.gallery.innerHTML = exercisesMarkup(cards, 'favorites');
+
+elements.gallery.addEventListener('click', favoritesRemoveHandler);
+elements.gallery.addEventListener('click', handleExercise);
+
+function favoritesRemoveHandler(e) {
+  if (!e.target.closest('[data-delete]')) return;
+  const cardId = e.target.closest('.main-item_card-exercises').dataset.id;
+  removeFavorites(cardId);
+  const cards = JSON.parse(localStorage.getItem(common.LS_KEY_FAVORITES)) ?? [];
+  elements.gallery.innerHTML = exercisesMarkup(cards, 'favorites');
+}
