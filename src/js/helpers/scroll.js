57be902f88dd54efca7fc79b552cf589b8439c78
filimon4uck <134231431+filimon4uck@users.noreply.github.com>
2023-12-16 12:32:07 +0100

    fix scroll top and scroll to element

--- src/js/helpers/scroll.js
+++ src/js/helpers/scroll.js
@@ -1,8 +1,29 @@
+// function scrollToTopOrElement(target) {
+//   if (target === 'top') {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+//   } else {
+//     target.scrollIntoView({ behavior: 'smooth' });
+//   }
+// }
 function scrollToTopOrElement(target) {
   if (target === 'top') {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   } else {
-    target.scrollIntoView({ behavior: 'smooth' });
+    const element = document.querySelector(target);
+    if (element) {
+      const elementRect = element.getBoundingClientRect();
+      if (
+        elementRect.top >= 0 &&
+        elementRect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight)
+      ) {
+        return;
+      }
+      window.scrollBy({
+        top: elementRect.top - 20,
+        behavior: 'smooth',
+      });
+    }
   }
 }
 export default scrollToTopOrElement;
