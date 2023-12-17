fd21d77073d4f76745e916a43d7e49861db19279
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-17 20:22:37 +0200

    add throttle to scroll

--- src/favorites.js
+++ src/favorites.js
@@ -1,11 +1,12 @@
 import 'modern-normalize';
+import throttle from 'lodash.throttle';
 import { favorites } from './js/services/favorites-page';
 import { elements } from './js/elements';
 import { handleScrollTop, handleScrollTopBtnShow, handlerOpenMenu } from './js/handlers';
 
+window.addEventListener('scroll', throttle(handleScrollTopBtnShow, 500))
 elements.btnOpenBurger.addEventListener('click', handlerOpenMenu);
 elements.btnCloseBurger.addEventListener('click', handlerOpenMenu);
-window.addEventListener('scroll', handleScrollTopBtnShow);
 elements.scrollTopBtn.addEventListener('click', handleScrollTop);
 
 favorites.load()
