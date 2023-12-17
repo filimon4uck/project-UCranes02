b752a2e4b0e2e28bd9851ca017d3b7a90c037448
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-17 12:39:41 +0200

    favorites interactivity complete

--- src/favorites.js
+++ src/favorites.js
@@ -1,23 +1,11 @@
 import 'modern-normalize';
-
+import { favorites } from './js/services/favorites-page';
 import { elements } from './js/elements';
-import removeFavorites from './js/helpers/favorites/remove-favorites';
-import handleExercise from './js/handlers/exercise-handler';
-import { renderFavorites } from './js/renderers';
-import handleFavoritePagination from './js/handlers/favorites-pagination-handler';
-import { handlerOpenMenu } from './js/handlers/index';
+import { handleScrollTop, handleScrollTopBtnShow, handlerOpenMenu } from './js/handlers';
 
-elements.pagination.addEventListener('click', handleFavoritePagination);
 elements.btnOpenBurger.addEventListener('click', handlerOpenMenu);
 elements.btnCloseBurger.addEventListener('click', handlerOpenMenu);
-elements.gallery.addEventListener('click', favoritesRemoveHandler);
-elements.gallery.addEventListener('click', handleExercise);
+window.addEventListener('scroll', handleScrollTopBtnShow);
+elements.scrollTopBtn.addEventListener('click', handleScrollTop);
 
-function favoritesRemoveHandler(e) {
-  if (!e.target.closest('[data-delete]')) return;
-  const cardId = e.target.closest('.link-exercise-card').dataset.id;
-  removeFavorites(cardId);
-  renderFavorites();
-}
-
-renderFavorites();
+favorites.load()
--- src/js/helpers/favorites/remove-favorites.js
+++
@@ -1,9 +0,0 @@
-function removeFavorites(id) {
-  const favoritesData = localStorage.getItem('favorites');
-  const parsedData = JSON.parse(favoritesData);
-  const index = parsedData.map(obj => obj._id).indexOf(id);
-  parsedData.splice(index, 1);
-  localStorage.setItem('favorites', JSON.stringify(parsedData));
-}
-
-export default removeFavorites;
