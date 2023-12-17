ad11a4420bdc3f361191e803725eb285a90ef12e
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-17 18:42:14 +0200

    renderers and styles update

--- src/js/renderers/render-favorites.js
+++ src/js/renderers/render-favorites.js
@@ -1,11 +1,18 @@
-import { common } from '../common';
 import { elements } from '../elements';
 import { renderPagination } from '../helpers/pagination';
 import { exercisesMarkup } from '../templates';
 
 function renderFavorites(array, page, totalPages) {
-  elements.gallery.innerHTML = exercisesMarkup(array, 'favorites');
-  renderPagination(page, totalPages);
+  elements.gallery.classList.add('unmounting');
+  if (!array.length) {
+    elements.gallery.innerHTML =
+      "<p class='list-empty'>It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>";
+  } else {
+    elements.gallery.innerHTML = exercisesMarkup(array, 'favorites');
+    renderPagination(page, totalPages);
+  }
+  elements.gallery.classList.remove('unmounting');
+  setTimeout(() => {}, 0);
 }
 
 export default renderFavorites;
--- src/js/renderers/render-subfilters.js
+++ src/js/renderers/render-subfilters.js
@@ -6,13 +6,22 @@ import { common } from '../common';
 import { renderPagination } from '../helpers/pagination';
 
 async function renderSubfilters() {
+  elements.gallery.classList.add('unmounting');
   try {
     const data = await exercisesApi.getFilters();
-    elements.gallery.innerHTML = subfiltersMarkup(data.results);
+    if (data.page > data.totalPages) {
+      gallery.changePage(data.totalPages);
+      return;
+    }
+    elements.gallery.innerHTML =
+      subfiltersMarkup(data.results) ||
+      `<img src="/img/no-foto.jpeg" alt="Not found">`;
     renderPagination(Number(data.page), data.totalPages);
   } catch {
     showError(common.ERROR_MESSAGE);
-    elements.gallery.innerHTML = '<p>Nothing was found<p/>';
+    elements.gallery.innerHTML = `<img src="/img/no-foto.jpeg" alt="Not found">`;
+  } finally {
+    elements.gallery.classList.remove('unmounting');
   }
 }
 
