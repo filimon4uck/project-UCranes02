669e9080b3aacde7dde26a3ff720915b44c1a53e
Iryna Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-12-17 08:23:54 +0200

    bugfix

--- src/js/handlers/burget-menu-handler.js
+++ src/js/handlers/burget-menu-handler.js
@@ -2,11 +2,11 @@ import { elements } from '../elements';
 function handlerOpenMenu(evt) {
   if (evt.currentTarget === elements.btnOpenBurger) {
     elements.burgerMenu.classList.remove('is-hidden');
-    document.querySelector('body').classList.add('scroll-fixed');
+    document.querySelector('html').classList.add('no-scroll');
   }
   if (evt.currentTarget === elements.btnCloseBurger) {
     elements.burgerMenu.classList.add('is-hidden');
-    document.querySelector('body').classList.remove('scroll-fixed');
+    document.querySelector('html').classList.remove('no-scroll');
   }
 }
 export default handlerOpenMenu;
